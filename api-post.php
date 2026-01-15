500a8f64671e1e5f316062e9a3b84202bafbe108
Alan Barzanti <alan.barzanti@studio.unibo.it>
2026-01-15 17:19:51 +0100

    For now post.php is finished

--- api-post.php
+++ api-post.php
@@ -2,15 +2,21 @@
 require_once 'bootstrap.php';
 if (isset($_GET['id'])) {
         $id = intval($_GET['id']);
-        $postArray = $dbh->getPost($id);
-    if (!$postArray || count($postArray) === 0) {
+        $post = $dbh->getPost($id);
+    if (!$post) {
         http_response_code(404);
         echo json_encode(['error' => 'Post non trovato']);
         exit;
     }
-    $post = $postArray[0];
     $post['foto'] = UPLOAD_DIR . $post['foto'];
     $post['partecipanti'] = $dbh->getMembersFromPost($id);
+    $post['numero_partecipanti'] = $dbh->getNumberOfMembersFromPost($id);
+    $post['commenti'] = $dbh->getCommentsFromPost($id);
+    for($i = 0; $i < count($post['commenti']); $i++){
+        $user = $dbh->getUserFromComment($post["commenti"][$i]['id']);
+        $post["commenti"][$i]['username'] = $user['username'];
+        $post["commenti"][$i]['id_utente'] = $user['id'];
+    }
 } else {
     $post = $dbh->getActivePosts();
     for($i = 0; $i < count($post); $i++){
--- db/popola_db.sql
+++ db/popola_db.sql
@@ -16,6 +16,12 @@ INSERT INTO CATEGORIA (id, nome_categoria) VALUES
 (3, 'sport');
 
 INSERT INTO POST (foto, titolo, descrizione, data_ora, posti_disponibili, provincia, comune, indirizzo, id_categoria, id_creatore) VALUES
-('img_prova.jpeg', 'Festa universitaria', 'grande festa yuhhuuu', '2026-01-20 21:00:00', 50, 'Milano', 'Milano', 'Via Roma 10', 1, 1),
-('img_prova.jpeg', 'Gruppo studio', 'grande studio nooooo', '2026-01-18 14:30:00', 20, 'Torino', 'Torino', 'Corso Vittorio 25', 2, 2),
-('img_prova.jpeg', 'Partita di beach volley', 'grande partita yuuhhuuuu', '2026-01-31 10:00:00', 15, 'Roma', 'Roma', 'Piazza Navona 5', 3, 2);
+(1, 'img_prova.jpeg', 'Festa universitaria', 'grande festa yuhhuuu', '2026-01-20 21:00:00', 50, 'Milano', 'Milano', 'Via Roma 10', 1, 1),
+(2, 'img_prova.jpeg', 'Gruppo studio', 'grande studio nooooo', '2026-01-18 14:30:00', 20, 'Torino', 'Torino', 'Corso Vittorio 25', 2, 2),
+(3, 'img_prova.jpeg', 'Partita di beach volley', 'grande partita yuuhhuuuu', '2026-01-31 10:00:00', 15, 'Roma', 'Roma', 'Piazza Navona 5', 3, 2);
+
+INSERT INTO ISCRIZIONE_POST (id_post, id_iscritto) VALUES
+(2, 1);
+
+INSERT INTO COMMENTO (id, testo, id_utente, id_post) VALUES
+(1, "Bellissimo evento, evvai!!!", 1, 2)
