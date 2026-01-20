5a50e12411c164278ac7073f48f0efb95494a2e5
Federico Mariani <federico.mariani3@studio.unibo.it>
2026-01-20 10:19:25 +0100

    fullfilled the db with popola_db

--- db/popola_db.sql
+++ db/popola_db.sql
@@ -12,17 +12,17 @@ INSERT INTO UTENTE (id, username, password_hash, nome, cognome, numero_telefono,
 (11, 'saraxpro', '$2y$10$CJmJxWNXqUyfJz3oqnbqF.fMT0qI4p34B34puS33y0So/nkUht2XS', 'Sara', 'Grigi', '3384308888', 'sara@gmail.com', 'user_696e5d06bf0ea.png', 'USER', 0),
 (12, 'george', '$2y$10$eNk0VTsW6PqNuSYarsQlx.7oNRL0TV6OOJA.DpEY9uE15u3oCP5XC', 'Giorgia', 'Molari', '123445678909', 'giorgia@gmail.com', 'user_696e5d2a6bba2.png', 'USER', 0);
 
--- INSERT INTO CATEGORIA (id, nome_categoria) VALUES
--- (1, 'festa'),
--- (2, 'studio'),
--- (3, 'sport');
+INSERT INTO CATEGORIA (id, nome_categoria) VALUES
+(1, 'festa'),
+(2, 'studio'),
+(3, 'sport');
 
--- INSERT INTO TIPO_NOTIFICA (id, tipologia) VALUES
--- (1, 'NUOVO_LIKE_A_POST'),
--- (2, 'NUOVA_ISCRIZIONE_A_POST'),
--- (3, 'NUOVO_COMMENTO_A_POST'),
--- (4, 'POST_ELIMINATO'),
--- (5, 'UTENTE_DISISCRITTO_DA_TUO_POST');
+INSERT INTO TIPO_NOTIFICA (id, tipologia) VALUES
+(1, 'NUOVO_LIKE_A_POST'),
+(2, 'NUOVA_ISCRIZIONE_A_POST'),
+(3, 'NUOVO_COMMENTO_A_POST'),
+(4, 'POST_ELIMINATO'),
+(5, 'UTENTE_DISISCRITTO_DA_TUO_POST');
 
 INSERT INTO POST
 (id, titolo, descrizione, data_ora, posti_disponibili, indirizzo, citta, comune, provincia, id_categoria, foto, id_creatore)
@@ -102,93 +102,231 @@ VALUES
  'Evento dedicato al networking tra studenti e professionisti, con tavoli tematici e opportunità di conoscere persone del settore.',
  '2026-02-18 18:30:00', 50, 'Centro Conferenze', 'Milano', 'Milano', 'MI', 1, 'networking.jpg', 5);
 
--- INSERT INTO COMMENTO (testo, id_utente, id_post) VALUES
--- -- POST 1
--- ('Ottima idea per iniziare l università', 3, 1),
--- ('Perfetto per conoscere nuove persone', 7, 1),
--- ('Evento ideale per le matricole', 10, 1),
+INSERT INTO COMMENTO (testo, id_utente, id_post) VALUES
+-- POST 1
+('Ottima idea per iniziare l università', 3, 1),
+('Perfetto per conoscere nuove persone', 7, 1),
+('Evento ideale per le matricole', 10, 1),
 
--- -- POST 2
--- ('Aperitivo rilassante dopo le lezioni', 2, 2),
--- ('Locale comodo vicino al campus', 6, 2),
--- ('Ottima occasione per socializzare', 11, 2),
+-- POST 2
+('Aperitivo rilassante dopo le lezioni', 2, 2),
+('Locale comodo vicino al campus', 6, 2),
+('Ottima occasione per socializzare', 11, 2),
 
--- -- POST 3
--- ('Finalmente una festa post esami', 4, 3),
--- ('Serata semplice ma divertente', 9, 3),
--- ('Perfetta per staccare dallo studio', 1, 3),
+-- POST 3
+('Finalmente una festa post esami', 4, 3),
+('Serata semplice ma divertente', 9, 3),
+('Perfetta per staccare dallo studio', 1, 3),
 
--- -- POST 4
--- ('Karaoke sempre una bella idea', 8, 4),
--- ('Serata diversa dal solito', 12, 4),
--- ('Ottimo per divertirsi insieme', 5, 4),
+-- POST 4
+('Karaoke sempre una bella idea', 8, 4),
+('Serata diversa dal solito', 12, 4),
+('Ottimo per divertirsi insieme', 5, 4),
 
--- -- POST 5
--- ('Bellissima atmosfera internazionale', 6, 5),
--- ('Ottimo evento per studenti Erasmus', 2, 5),
--- ('Perfetto per fare nuove amicizie', 9, 5),
+-- POST 5
+('Bellissima atmosfera internazionale', 6, 5),
+('Ottimo evento per studenti Erasmus', 2, 5),
+('Perfetto per fare nuove amicizie', 9, 5),
 
--- -- POST 6
--- ('Gruppo studio molto utile', 1, 6),
--- ('Perfetto per chiarire i dubbi', 7, 6),
--- ('Analisi diventa più semplice insieme', 11, 6),
+-- POST 6
+('Gruppo studio molto utile', 1, 6),
+('Perfetto per chiarire i dubbi', 7, 6),
+('Analisi diventa più semplice insieme', 11, 6),
 
--- -- POST 7
--- ('Ripasso fatto molto bene', 3, 7),
--- ('Utile per prepararsi all esame', 10, 7),
--- ('Ottimo confronto tra studenti', 6, 7),
+-- POST 7
+('Ripasso fatto molto bene', 3, 7),
+('Utile per prepararsi all esame', 10, 7),
+('Ottimo confronto tra studenti', 6, 7),
 
--- -- POST 8
--- ('Argomenti spiegati in modo chiaro', 4, 8),
--- ('Diritto sembra meno complicato', 12, 8),
--- ('Ottima iniziativa per lo studio', 8, 8),
+-- POST 8
+('Argomenti spiegati in modo chiaro', 4, 8),
+('Diritto sembra meno complicato', 12, 8),
+('Ottima iniziativa per lo studio', 8, 8),
 
--- -- POST 9
--- ('Informatica studiata meglio in gruppo', 5, 9),
--- ('Esercizi spiegati molto bene', 2, 9),
--- ('Ambiente collaborativo e utile', 11, 9),
+-- POST 9
+('Informatica studiata meglio in gruppo', 5, 9),
+('Esercizi spiegati molto bene', 2, 9),
+('Ambiente collaborativo e utile', 11, 9),
 
--- -- POST 10
--- ('Confronto utile per la tesi', 9, 10),
--- ('Ottimi consigli sulla stesura', 3, 10),
--- ('Incontro molto produttivo', 7, 10),
+-- POST 10
+('Confronto utile per la tesi', 9, 10),
+('Ottimi consigli sulla stesura', 3, 10),
+('Incontro molto produttivo', 7, 10),
 
--- -- POST 11
--- ('Fisica più semplice insieme', 6, 11),
--- ('Esercizi spiegati passo passo', 1, 11),
--- ('Sessione di studio efficace', 10, 11),
+-- POST 11
+('Fisica più semplice insieme', 6, 11),
+('Esercizi spiegati passo passo', 1, 11),
+('Sessione di studio efficace', 10, 11),
 
--- -- POST 12
--- ('Calcetto perfetto dopo le lezioni', 8, 12),
--- ('Partita divertente e rilassante', 4, 12),
--- ('Ottimo modo per fare sport', 12, 12),
+-- POST 12
+('Calcetto perfetto dopo le lezioni', 8, 12),
+('Partita divertente e rilassante', 4, 12),
+('Ottimo modo per fare sport', 12, 12),
 
--- -- POST 13
--- ('Pallavolo mista molto inclusiva', 2, 13),
--- ('Ambiente amichevole e sportivo', 9, 13),
--- ('Allenamento leggero ma divertente', 6, 13),
+-- POST 13
+('Pallavolo mista molto inclusiva', 2, 13),
+('Ambiente amichevole e sportivo', 9, 13),
+('Allenamento leggero ma divertente', 6, 13),
 
--- -- POST 14
--- ('Corsa ideale per scaricare stress', 11, 14),
--- ('Adatta a tutti i livelli', 5, 14),
--- ('Ottima iniziativa per studenti', 3, 14),
+-- POST 14
+('Corsa ideale per scaricare stress', 11, 14),
+('Adatta a tutti i livelli', 5, 14),
+('Ottima iniziativa per studenti', 3, 14),
 
--- -- POST 15
--- ('Allenarsi insieme motiva molto', 7, 15),
--- ('Palestra comoda e ben attrezzata', 1, 15),
--- ('Sessione utile e ben organizzata', 10, 15),
+-- POST 15
+('Allenarsi insieme motiva molto', 7, 15),
+('Palestra comoda e ben attrezzata', 1, 15),
+('Sessione utile e ben organizzata', 10, 15),
 
--- -- POST 16
--- ('Basket sempre una buona idea', 4, 16),
--- ('Partita divertente tra studenti', 8, 16),
--- ('Bel modo di socializzare', 12, 16),
+-- POST 16
+('Basket sempre una buona idea', 4, 16),
+('Partita divertente tra studenti', 8, 16),
+('Bel modo di socializzare', 12, 16),
 
--- -- POST 17
--- ('Yoga perfetto per rilassarsi', 6, 17),
--- ('Aiuta concentrazione e benessere', 2, 17),
--- ('Lezione molto piacevole', 9, 17),
+-- POST 17
+('Yoga perfetto per rilassarsi', 6, 17),
+('Aiuta concentrazione e benessere', 2, 17),
+('Lezione molto piacevole', 9, 17),
 
--- -- POST 18
--- ('Evento ben organizzato', 3, 18),
--- ('Contenuti interessanti e utili', 11, 18),
--- ('Partecipazione consigliata', 7, 18);
+-- POST 18
+('Evento ben organizzato', 3, 18),
+('Contenuti interessanti e utili', 11, 18),
+('Partecipazione consigliata', 7, 18);
+
+INSERT INTO ISCRIZIONE_POST (id_post, id_iscritto) VALUES
+-- POST 1
+(1, 1), (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),
+-- POST 2
+(2, 3), (2, 5), (2, 6), (2, 1), (2, 8),
+-- POST 3
+(3, 2), (3, 7), (3, 8), (3, 9), (3, 4),
+-- POST 4
+(4, 1), (4, 4), (4, 10), (4, 6), (4, 7),
+-- POST 5
+(5, 6), (5, 7), (5, 11), (5, 12), (5, 2),
+-- POST 6
+(6, 2), (6, 3), (6, 8), (6, 9),
+-- POST 7
+(7, 4), (7, 5), (7, 6), (7, 1),
+-- POST 8
+(8, 1), (8, 7), (8, 10),
+-- POST 9
+(9, 8), (9, 9), (9, 10), (9, 2),
+-- POST 10
+(10, 3), (10, 11), (10, 4),
+-- POST 11
+(11, 2), (11, 6), (11, 12),
+-- POST 12
+(12, 1), (12, 4), (12, 5), (12, 7),
+-- POST 13
+(13, 6), (13, 8),
+-- POST 14
+(14, 9), (14, 10), (14, 11),
+-- POST 15
+(15, 3), (15, 4),
+-- POST 16
+(16, 5), (16, 6), (16, 12),
+-- POST 17
+(17, 2), (17, 8),
+-- POST 18
+(18, 1), (18, 9);
+
+
+INSERT INTO LIKE_POST (id_post, id_utente) VALUES
+-- POST 1
+(1,1), (1,2), (1,3), (1,4), (1,5),
+-- POST 2
+(2,1), (2,3), (2,6), (2,8),
+-- POST 3
+(3,2), (3,4), (3,7), (3,9), (3,10),
+-- POST 4
+(4,1), (4,6), (4,7), (4,10),
+-- POST 5
+(5,2), (5,6), (5,7), (5,11), (5,12),
+-- POST 6
+(6,1), (6,2), (6,3),
+-- POST 7
+(7,4), (7,5), (7,6), (7,7),
+-- POST 8
+(8,1), (8,7), (8,10),
+-- POST 9
+(9,2), (9,8), (9,9), (9,10),
+-- POST 10
+(10,3), (10,4), (10,11),
+-- POST 11
+(11,2), (11,6), (11,12),
+-- POST 12
+(12,1), (12,4), (12,5), (12,7), (12,8),
+-- POST 13
+(13,6), (13,8),
+-- POST 14
+(14,9), (14,10), (14,11),
+-- POST 15
+(15,3), (15,4),
+-- POST 16
+(16,5), (16,6), (16,12),
+-- POST 17
+(17,2), (17,8),
+-- POST 18
+(18,1), (18,9), (18,10);
+
+INSERT INTO NOTIFICA
+(id_tipo_notifica, id_destinatario, data_ora, id_mittente, id_post, letto)
+VALUES
+-- LIKE (1)
+(1, 1, '2026-02-01 10:00:00', 2, 1, FALSE),
+(1, 3, '2026-02-01 10:05:00', 4, 2, FALSE),
+(1, 5, '2026-02-01 10:10:00', 6, 3, TRUE),
+(1, 7, '2026-02-01 10:15:00', 8, 4, FALSE),
+(1, 9, '2026-02-01 10:20:00', 10, 5, TRUE),
+(1, 11, '2026-02-01 10:25:00', 12, 6, FALSE),
+(1, 2, '2026-02-01 10:30:00', 1, 7, FALSE),
+(1, 4, '2026-02-01 10:35:00', 3, 8, TRUE),
+(1, 6, '2026-02-01 10:40:00', 5, 9, FALSE),
+(1, 8, '2026-02-01 10:45:00', 7, 10, TRUE),
+(1, 10, '2026-02-01 10:50:00', 9, 11, FALSE),
+(1, 12, '2026-02-01 10:55:00', 11, 12, TRUE),
+
+-- PARTECIPAZIONE (2)
+(2, 1, '2026-02-02 11:00:00', 3, 2, FALSE),
+(2, 2, '2026-02-02 11:05:00', 4, 3, FALSE),
+(2, 3, '2026-02-02 11:10:00', 5, 4, TRUE),
+(2, 4, '2026-02-02 11:15:00', 6, 5, FALSE),
+(2, 5, '2026-02-02 11:20:00', 7, 6, TRUE),
+(2, 6, '2026-02-02 11:25:00', 8, 7, FALSE),
+(2, 7, '2026-02-02 11:30:00', 9, 8, FALSE),
+(2, 8, '2026-02-02 11:35:00', 10, 9, TRUE),
+(2, 9, '2026-02-02 11:40:00', 11, 10, FALSE),
+(2, 10, '2026-02-02 11:45:00', 12, 11, TRUE),
+(2, 11, '2026-02-02 11:50:00', 1, 12, FALSE),
+(2, 12, '2026-02-02 11:55:00', 2, 13, TRUE),
+
+-- COMMENTO (3)
+(3, 1, '2026-02-03 12:00:00', 4, 3, FALSE),
+(3, 2, '2026-02-03 12:05:00', 5, 4, TRUE),
+(3, 3, '2026-02-03 12:10:00', 6, 5, FALSE),
+(3, 4, '2026-02-03 12:15:00', 7, 6, TRUE),
+(3, 5, '2026-02-03 12:20:00', 8, 7, FALSE),
+(3, 6, '2026-02-03 12:25:00', 9, 8, FALSE),
+(3, 7, '2026-02-03 12:30:00', 10, 9, TRUE),
+(3, 8, '2026-02-03 12:35:00', 11, 10, FALSE),
+(3, 9, '2026-02-03 12:40:00', 12, 11, TRUE),
+(3, 10, '2026-02-03 12:45:00', 1, 12, FALSE),
+(3, 11, '2026-02-03 12:50:00', 2, 13, TRUE),
+(3, 12, '2026-02-03 12:55:00', 3, 14, FALSE),
+
+-- DISISCRIZIONE (4)
+(4, 1, '2026-02-04 13:00:00', 5, 6, FALSE),
+(4, 2, '2026-02-04 13:05:00', 6, 7, TRUE),
+(4, 3, '2026-02-04 13:10:00', 7, 8, FALSE),
+(4, 4, '2026-02-04 13:15:00', 8, 9, TRUE),
+(4, 5, '2026-02-04 13:20:00', 9, 10, FALSE),
+(4, 6, '2026-02-04 13:25:00', 10, 11, TRUE),
+
+-- POST ELIMINATO (5)
+(5, 7, '2026-02-05 14:00:00', NULL, 12, FALSE),
+(5, 8, '2026-02-05 14:05:00', NULL, 13, FALSE),
+(5, 9, '2026-02-05 14:10:00', NULL, 14, TRUE),
+(5, 10, '2026-02-05 14:15:00', NULL, 15, FALSE),
+(5, 11, '2026-02-05 14:20:00', NULL, 16, TRUE),
+(5, 12, '2026-02-05 14:25:00', NULL, 17, FALSE);
